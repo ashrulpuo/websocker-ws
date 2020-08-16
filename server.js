99ef78ab9be709693b119d1d713f8e924215868a
Shahrin Nidzam <shahrin.nidzam@gmail.com>
2020-08-16 19:01:59 +0800

    update

--- server.js
+++ server.js
@@ -8,19 +8,20 @@ const PORT = process.env.PORT || 3001;
 const INDEX = path.join(__dirname, 'index.html');
 
 var clients = [];
+var allclient = [];
 
-function register(data, ws){
-  if(!clients[data.channel]){
+function register(data, ws) {
+  if (!clients[data.channel]) {
     clients[data.channel] = [];
   }
 
   clients[data.channel][data.id] = ws;
-  console.log(clients);
+  if (!allclient.includes(ws)) allclient.push(ws);
 }
 
-function message(data){
+function privateChannel(data) {
   var participants = clients[data.channel];
-  for(var user_id in participants){
+  for (var user_id in participants) {
     var send = {
       from: data.from,
       channel: data.channel,
@@ -30,15 +31,29 @@ function message(data){
   }
 }
 
-function assignProcess(data, ws = null){
-  try{
+function publicChannel(data){
+  var send = {
+    from: data.from,
+    channel: "public",
+    message: data.message
+  }
+  for(var i = 0; i < allclient.length; i++){
+    allclient[i].send(JSON.stringify(send));
+  }
+}
+
+function assignProcess(data, ws = null) {
+  try {
     var d = JSON.parse(data);
-    switch(d.type){
+    switch (d.type) {
       case "register":
         register(d.body, ws);
         break;
-      case "message":
-        message(d.body);
+      case "private":
+        privateChannel(d.body);
+        break;
+      case "public":
+        publicChannel(d.body);
         break;
     }
   } catch (e) {
@@ -46,13 +61,13 @@ function assignProcess(data, ws = null){
 }
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws, req) => {
-  ws.on('message', function(message){
+  ws.on('message', function (message) {
     assignProcess(message, ws);
   });
   ws.on('close', () => console.log('Client disconnected'));
